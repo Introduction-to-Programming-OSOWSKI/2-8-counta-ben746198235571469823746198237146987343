5a1decf3edd0923064a93f6610f5b1c424b27063
ben746198235571469823746198237146987343 <824516@mystma.org>
2023-03-31 14:50:48 -0500

    2

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ def countA(w):
     for i in range(0,len(w)):
         if w[i] == "a":
             num=num+1
-    print(str(num))
+    print("there is " + str(num)+ " a/'s in the word " + w + ".")
+
+
+countA("rat")
